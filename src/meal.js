093c2fdad294085eeba8d75e4561409c14effc77
Trey Marshall <marshall.trey92@yahoo.com>
2023-02-01 15:37:00 -0600

    Complete meal-test.js tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+ console.log(name) 
+ return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(food, ingredients) {  
+  if (ingredients.includes(food)) {
+    return ingredients
+  } else {
+    return ingredients.push(food)
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price) {
+  return price * .9
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var menuObject = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  } 
+  return menuObject
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
--- src/order.js
+++ src/order.js
@@ -1,6 +1,22 @@
+function takeOrder(order, delivery) {
+  if(delivery.length < 3) {
+    return delivery.push(order)
+  }
+}
 
+function refundOrder() {
+
+}
+
+function listItems() {
+
+}
+
+function searchOrder() {
+
+}
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder

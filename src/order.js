e92125defd66a528e8c2ce8f589ebe5ff7bbe4a9
Trey Marshall <marshall.trey92@yahoo.com>
2023-02-02 17:24:45 -0600

    Complete work on restaurant.js and refactor order.js

--- src/order.js
+++ src/order.js
@@ -5,22 +5,24 @@ function takeOrder(order, delivery) {
 }
 
 function refundOrder(orderNumber, delivery) {
-  for(var i = 0; i < delivery.length; i++)
-  if(orderNumber === delivery[i].orderNumber) {
-    return delivery.splice(i, 1)
+  for(var i = 0; i < delivery.length; i++) {
+    if(orderNumber === delivery[i].orderNumber) {
+      return delivery.splice(i, 1)
+    }
   }
 }
 
 function listItems(deliveryOrders) {
   var order = []
-  for (var i = 0; i < deliveryOrders.length; i++) 
-  order.push(deliveryOrders[i].item)
-  return order.join(", ")
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    order.push(deliveryOrders[i].item)
+  }
+    return order.join(", ")
 }
 
 function searchOrder(order, food) {
-  for(var i = 0; i < order.length; i++) 
-    if (order[i].item === food) {
+  for(var i = 0; i < order.length; i++) { 
+    if (order[i].item === food) 
       return true
     }
       return false
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,46 @@
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {breakfast: [], lunch: [], dinner: []}
+  }
 
+}
 
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item)) {
+    restaurant.menus[item.type].push(item)
+  }
+}
+
+function removeMenuItem(restaurant, food, item) {
+  if (item === "breakfast") {
+    for (var i = 0; i < restaurant.menus.breakfast.length; i++) {
+      if (food === restaurant.menus.breakfast[i].name) {
+        restaurant.menus.breakfast.splice(i, 1)
+        return `No one is eating our ${food} - it has been removed from the ${item} menu!`
+      }
+    }
+  }
+  if (item === "dinner") {
+    for (var i = 0; i < restaurant.menus.dinner.length; i++) {
+      if (food === restaurant.menus.dinner[i].name) {
+        restaurant.menus.dinner.splice(i, 1)
+        return `No one is eating our ${food} - it has been removed from the ${item} menu!`
+      }
+    }
+  }
+  if (item === "dinner") {
+    for (var i = 0; i < restaurant.menus.dinner.length; i++) {
+      if (food === restaurant.menus.dinner[i].name) {
+        restaurant.menus.dinner.splice(i, 1)
+        return `No one is eating our ${food} - it has been removed from the ${item} menu!`
+      }
+    }
+  }
+  return `Sorry, we don't sell ${food}, try adding a new recipe!`
+}
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem
 }

28600b15857c5c551bc7e19194e97a6e4098f8fc
Trey Marshall <marshall.trey92@yahoo.com>
2023-02-03 14:57:59 -0600

    Complete extra challenge in chef-test.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,5 +1,21 @@
 class Chef {
-  constructor() {
+  constructor(name, restaurant) {
+    this.name = name
+    this.restaurant = restaurant
+    
+  }
+  greetCustomer(name, boolean) {
+    if (boolean === true) {
+      return `Good morning, ${name}!`
+    }
+      return `Hello, ${name}!`
+  }
+
+  checkForFood(food) {
+    if (food.type === "lunch" || food.name === "Kouign Amann") {
+      return `Sorry, we aren't serving ${food.name} today.`
+    } 
+      return `Yes, we're serving ${food.name} today!`
     
   }
 }
